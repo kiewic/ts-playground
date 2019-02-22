9e40e8a8e84bba7d69c7ecb5a1900f254428b17c
Gilberto Stankiewicz Chavez <gistanki@microsoft.com>
2019-02-22 01:06:51 -0800

    Add promise project

---
+++ promise/build/index.js
@@ -0,0 +1,19 @@
+let promise1 = new Promise((resolve, reject) => {
+    resolve('a string');
+});
+let promise2 = new Promise((resolve, reject) => {
+    reject({ foo: true, bar: false });
+});
+function onfulfilled(value) {
+    console.log(value);
+}
+// `reject` accepts `any` argument. This results from the fact that a promise
+// can be rejected by throwing inside `then` or `catch` (this is a preferable way
+// to reject existing promise), and this cannot be handled by typing system.
+//
+// Source: https://stackoverflow.com/a/50071254/27211
+function onrejected(reason) {
+    console.log(reason);
+}
+promise1.then(onfulfilled, onrejected);
+promise2.then(onfulfilled, onrejected);
